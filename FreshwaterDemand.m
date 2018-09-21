726578bddc652b4fc014fe9a95bf004c92a76af3
WaterEnergyWork <43465651+WaterEnergyWork@users.noreply.github.com>
2018-09-21 12:42:56 +0300

    Add files via upload

---
+++ FreshwaterDemand.m
@@ -0,0 +1,91 @@
+%This script calculates freshwater demand of each thermal power plant in
+%the specific country. The first part of the code calculates the specific
+%freshwater withdrawals, the second part calculates the specific freshwater
+%consumption.
+
+clear WaterPower
+WaterPower=zeros(size(NumberPowerPlant,1),3);
+for i=1:size(NumberPowerPlant,1)
+   if SeaCooling(NumberPowerPlant==i)==0 %for freshwater-cooled power plants
+      
+%TypeOfFuel=1 for coal power plants; TypeOfFuel=2 for gas power plants;
+%TypeOfFuel=3 for nuclear power plants; TypeOfFuel=4 for oil power plants
+%FlhCoal2015, FlhGT2015, FlhNuclear2015, FlhOil2015 - Full Load Hours of
+%the specific power plant in 2015, obtained from IEA (2015)
+
+            if TypeOfFuel(NumberPowerPlant==i)==1
+            FLHcoeff=FlhCoal2015(NodeNumberFLH==NodeTotal(NumberPowerPlant==i));
+            FLHcoeff(isnan(FLHcoeff))=0;    
+           elseif TypeOfFuel(NumberPowerPlant==i)==2
+            FLHcoeff=FlhGT2015(NodeNumberFLH==NodeTotal(NumberPowerPlant==i));
+            FLHcoeff(isnan(FLHcoeff))=0; 
+           elseif TypeOfFuel(NumberPowerPlant==i)==3
+            FLHcoeff=FlhNuclear2015(NodeNumberFLH==NodeTotal(NumberPowerPlant==i));
+            FLHcoeff(isnan(FLHcoeff))=0; 
+           elseif TypeOfFuel(NumberPowerPlant==i)==4
+            FLHcoeff=FlhOil2015(NodeNumberFLH==NodeTotal(NumberPowerPlant==i));
+            FLHcoeff(isnan(FLHcoeff))=0; 
+            end
+
+%Extraction of water withdrawal factors for the specific thermal power plant, this code uses a table created by the authors, based on Macknick et al.(2012)
+%WithdMedian, WithdMin, WithdMax - water use intensity (withdrawal), based on Macknick et al.(2012)
+%Examples of Technology: combined cycle, steam, subcritial and supercritical (for coal power plants), etc.
+
+           WatercoeffMedian=WithdMedian(TypeOfFuelWater==TypeOfFuel(NumberPowerPlant==i) & NumberCoolingWater==NumberCooling(NumberPowerPlant==i) & NumberTechnologyWater==NumberTechnology(NumberPowerPlant==i));
+           WatercoeffMin=WithdMin(TypeOfFuelWater==TypeOfFuel(NumberPowerPlant==i) & NumberCoolingWater==NumberCooling(NumberPowerPlant==i) & NumberTechnologyWater==NumberTechnology(NumberPowerPlant==i));
+           WatercoeffMax=WithdMax(TypeOfFuelWater==TypeOfFuel(NumberPowerPlant==i) & NumberCoolingWater==NumberCooling(NumberPowerPlant==i) & NumberTechnologyWater==NumberTechnology(NumberPowerPlant==i));
+                
+           WaterPower(i,1)=ActiveCapacityMW(NumberPowerPlant==i)*WatercoeffMedian*FLHcoeff;
+           WaterPower(i,2)=ActiveCapacityMW(NumberPowerPlant==i)*WatercoeffMin*FLHcoeff;
+           WaterPower(i,3)=ActiveCapacityMW(NumberPowerPlant==i)*WatercoeffMax*FLHcoeff;
+           
+  end
+                    clear WatercoeffMedian;
+                    clear WatercoeffMin;
+                    clear WatercoeffMax;
+                    clear FLHcoeff;
+end
+
+clear WaterPower
+WaterPower=zeros(size(NumberPowerPlant,1),3);
+for i=1:size(NumberPowerPlant,1)
+   if SeaCooling(NumberPowerPlant==i)==0 %for freshwater-cooled power plants
+      
+%TypeOfFuel=1 for coal power plants; TypeOfFuel=2 for gas power plants;
+%TypeOfFuel=3 for nuclear power plants; TypeOfFuel=4 for oil power plants
+%FlhCoal2015, FlhGT2015, FlhNuclear2015, FlhOil2015 - Full Load Hours of
+%the specific power plant in 2015, obtained from IEA (2015)
+
+            if TypeOfFuel(NumberPowerPlant==i)==1
+            FLHcoeff=FlhCoal2015(NodeNumberFLH==NodeTotal(NumberPowerPlant==i));
+            FLHcoeff(isnan(FLHcoeff))=0;    
+           elseif TypeOfFuel(NumberPowerPlant==i)==2
+            FLHcoeff=FlhGT2015(NodeNumberFLH==NodeTotal(NumberPowerPlant==i));
+            FLHcoeff(isnan(FLHcoeff))=0; 
+           elseif TypeOfFuel(NumberPowerPlant==i)==3
+            FLHcoeff=FlhNuclear2015(NodeNumberFLH==NodeTotal(NumberPowerPlant==i));
+            FLHcoeff(isnan(FLHcoeff))=0; 
+           elseif TypeOfFuel(NumberPowerPlant==i)==4
+            FLHcoeff=FlhOil2015(NodeNumberFLH==NodeTotal(NumberPowerPlant==i));
+            FLHcoeff(isnan(FLHcoeff))=0; 
+            end
+
+%Extraction of water consumption factors for the specific thermal power
+%plant, this code uses a table created by the authors, based on Macknick et al.(2012)
+%ConsMedian, ConsMin, ConsMax - water use intensity (consumption)
+%Examples of Technology: combined cycle, steam, subcritial and supercritical (for coal power plants), etc.
+
+           WatercoeffMedian=ConsMedian(TypeOfFuelWater==TypeOfFuel(NumberPowerPlant==i) & NumberCoolingWater==NumberCooling(NumberPowerPlant==i) & NumberTechnologyWater==NumberTechnology(NumberPowerPlant==i));
+           WatercoeffMin=ConsMin(TypeOfFuelWater==TypeOfFuel(NumberPowerPlant==i) & NumberCoolingWater==NumberCooling(NumberPowerPlant==i) & NumberTechnologyWater==NumberTechnology(NumberPowerPlant==i));
+           WatercoeffMax=ConsMax(TypeOfFuelWater==TypeOfFuel(NumberPowerPlant==i) & NumberCoolingWater==NumberCooling(NumberPowerPlant==i) & NumberTechnologyWater==NumberTechnology(NumberPowerPlant==i));
+                
+           WaterPower(i,1)=ActiveCapacityMW(NumberPowerPlant==i)*WatercoeffMedian*FLHcoeff;
+           WaterPower(i,2)=ActiveCapacityMW(NumberPowerPlant==i)*WatercoeffMin*FLHcoeff;
+           WaterPower(i,3)=ActiveCapacityMW(NumberPowerPlant==i)*WatercoeffMax*FLHcoeff;
+           
+  end
+                    clear WatercoeffMedian;
+                    clear WatercoeffMin;
+                    clear WatercoeffMax;
+                    clear FLHcoeff;
+end
